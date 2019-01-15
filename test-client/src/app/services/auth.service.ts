9bc44537e3f10539a24895aa92b8207b305e5b05
torchlard <kidult.family@gmail.com>
2019-01-15 23:33:40 +0800

    new

--- test-client/src/app/services/auth.service.ts
+++ test-client/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { shareReplay } from 'rxjs/operators'
+import * as moment from 'moment'
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,38 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string){
-    return this.http.post<User>('/api/login', {email, password}).shareReplay()
-      
+    return this.http.post<User>('/api/login', {email, password})
+      .do(res => this.setSession)
+      .shareReplay()
   }
+
+  private setSession(authResult){
+    const expiresAt = moment().add(authResult.expiresIn, 'second')
+    localStorage.setItem('id_token', authResult.idToken)
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()))
+  }
+
+  logout() {
+    localStorage.removeItem('id_token')
+    localStorage.removeItem('expires_at')
+  }
+
+  public isLoggedIn(){
+    return moment().isBefore(this.getExpiration())
+  }
+
+  isLoggedOut(){
+    return moment().isBefore(this.getExpiration())
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at")
+    const expiresAt = JSON.parse(expiration)
+
+    return moment(expiresAt)
+  }
+  
+  
 }
 
 
--- test-server/index.mjs
+++ test-server/index.mjs
@@ -1,35 +1,115 @@
 // import {Request, Response} from "express"
-import * as express from "express"
-import * as jwt from 'jsonwebtoken'
-import * as bodyParser from 'body-parser'
-import * as cookieParser from 'cookie-parser'
+import express from "express"
+import * as jwksRsd from 'jwks-rsa'
+import * as expressJwt from 'express-jwt'
+import jwt from 'jsonwebtoken'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import * as fs from 'fs'
+
+const privateKey = 'secretkey'
 
 const app = express()
 app.use(bodyParser.json())
 
+
+app.route('/test').get((req,res) => {
+  res.json({'reply': 'hel'})
+})
+
 app.route('/api/login').post(loginRoute)
+// checkIfAuthenticated: middleware checking
 
-const rsa_key = fs.readFileSync('./demos/private.key')
+app.route('/api/lessons').get(verifyToken, (req,res) => {
 
-export function loginRoute(req, res){
+  jwt.verify(req.token, privateKey, (err, authData) => {
+    if (err){
+      res.sendStatus(403)
+    } else {
+      res.json({lesson: ['a', 'b', 'c'], authData })
+    }
+  })
+})
+
+
+// const rsa_key = fs.readFileSync('./demos/private.key')
+
+
+function loginRoute(req, res){
   const email = req.body.email
   const password = req.body.password
 
-  if (validateEmailAndPassword()){
-    const userId = findUserIdForEmail(email)
+  if (email == 'abc', password == '123456' ){
+    const user = {
+      id: 1,
+      username: 'brad',
+      email: 'brad@gmail.com'
+    }
 
-    const jwtBearerToken = jwt.sign({}, rsa_key, {
-      algorithm: 'RSA256',
-      expiresIn: 120,
-      subject: userId
+    // const jwtBearerToken = 
+    jwt.sign({user}, privateKey, {expiresIn: '20s'}, (err,token) => {
+      // algorithm: 'RS256',
+      // expiresIn: 120,
+      // subject: userId
+      res.json({
+        token
+      })
     })
+
+    // res.cookie("SESSIONID", jwtBearerToken, {httpOnly: true}) // secure: true
+
+    // res.status(200).join({
+    //   idToken: jwtBearerToken,
+    //   // expiresIn: 
+    // })
+    
   } else {
     res.sendStatus(401)
   }
-
   
 }
+
+// verifyToken
+// const verifyToken = expressJwt({
+//   secret: jwksRsd.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,  // library won't make >10 requests / min
+//     jwksUri: "./demos/jwks.json"
+//   }),
+//   algorithm: ['RS256']
+// })
+
+
+function verifyToken(req,res,next) {
+  console.log(req.headers);
+  const bearerHeader = req.headers['authorization']
+
+  if (typeof bearerHeader !== 'undefined'){
+    const bearer = bearerHeader.split(" ")
+    const bearerToken = bearer[1]
+    req.token = bearerToken
+    next()
+
+  } else {
+    // forbidden
+    res.sendStatus(403)
+  }
+}
+
+
+
+const port = process.env.PORT || 4001;
+app.listen(port, err => {
+  if (err){
+    console.error(err);
+  } else {
+    console.log('listening on port', port);
+  }
+})
+
+
+
+
 
 
 
